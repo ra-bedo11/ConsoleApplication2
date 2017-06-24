f4f4b0a42275731d616151348190df085314a1be
yoyo <yoyo@PC1>
2017-06-24 20:46:39 +0300

    Add project files.

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool res;
+            int myvar1 = 6, myvar2 = 9;
+            res = myvar1 == myvar2;
+            Console.WriteLine("the result {0}",res);
+        }
+    }
+}
